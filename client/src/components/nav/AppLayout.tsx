5b9a582be6fa19197cdfb035f0efe4fe2fb7dda0
Robert D Tech <mannyharley20@gmail.com>
2024-10-19 12:06:31 -0700

    hide pages from users

--- client/src/components/nav/AppLayout.tsx
+++ client/src/components/nav/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { useIsAuthenticated } from '@azure/msal-react';
+import { useMsal } from '@azure/msal-react';
 import { Layout, Menu } from 'antd';
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
@@ -7,11 +7,14 @@ const { Header, Content, Footer } = Layout;
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   const location = useLocation();
-  const isAuthenticated = useIsAuthenticated();
+  const { accounts } = useMsal();
 
-  // Determine the selected menu item based on the current route
+  let roles = '';
+  if (accounts[0]?.idTokenClaims?.extension_applicationRoles)
+    roles = accounts[0].idTokenClaims.extension_applicationRoles as string;
   const selectedKeys = [location.pathname];
-
+  let isAdmin = false;
+  if (roles.includes('ns-admin')) isAdmin = true;
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
@@ -24,36 +27,38 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
         </div>
         <Menu theme='dark' mode='horizontal' selectedKeys={selectedKeys}>
           <Menu.Item key='/'>
-            <Link to='/'>Home</Link>
+            <Link to='/'>Library</Link>
           </Menu.Item>
-          <Menu.Item key='/models'>
-            <Link to='/models'>Model Manager</Link>
-          </Menu.Item>
-          <Menu.Item key='/library'>
-            <Link to='/library'>Library</Link>
-          </Menu.Item>
-          <Menu.Item key='/ai-reader'>
-            <Link to='/ai-reader'>AI Enhanced Reader</Link>
-          </Menu.Item>
-          <Menu.Item key='/profile-loras'>
-            <Link to='/profile-loras'>Profile Loras</Link>
-          </Menu.Item>
-          <Menu.Item key='/profiles'>
-            <Link to='/profiles'>Profiles</Link>
-          </Menu.Item>
-          <Menu.Item key='/associate-loras'>
-            <Link to='/associate-loras'>Associate LoRAs</Link>
-          </Menu.Item>
-          <Menu.Item key='/chapter-generation'>
-            <Link to='/chapter-generation'>Chapter Generation</Link>
-          </Menu.Item>
+          {isAdmin && (
+            <>
+              <Menu.Item key='/models'>
+                <Link to='/models'>Model Manager</Link>
+              </Menu.Item>
+              <Menu.Item key='/ai-reader'>
+                <Link to='/ai-reader'>AI Enhanced Reader</Link>
+              </Menu.Item>
+              <Menu.Item key='/profile-loras'>
+                <Link to='/profile-loras'>Profile Loras</Link>
+              </Menu.Item>
+              <Menu.Item key='/profiles'>
+                <Link to='/profiles'>Profiles</Link>
+              </Menu.Item>
+              <Menu.Item key='/chapter-generation'>
+                <Link to='/chapter-generation'>Chapter Generation</Link>
+              </Menu.Item>
+            </>
+          )}
           <Menu.Item key='/user'>
             <Link to='/user'>User</Link>
           </Menu.Item>
         </Menu>
       </Header>
       <Content style={{ padding: '50px', marginTop: 64 }}>{children}</Content>
-      <Footer style={{ textAlign: 'center' }}>©2023 My App</Footer>
+      <Footer style={{ textAlign: 'center' }}>
+        Crafted with the wisdom of ages, where tales come to life and
+        imagination knows no bounds. Venture forth, for every passage leads to
+        new horizons.
+      </Footer>
     </Layout>
   );
 };
